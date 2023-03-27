6a43f88eb28270ea6d49df91cba147eeea58c0a0
saiful8183 <Manha103019@gmail.com>
2023-03-27 18:22:56 -0400

    Temporary transfer 5

---
+++ ActionItemsQA/src/actionItems/java.java
@@ -0,0 +1,61 @@
+package actionItems;
+
+public class java {
+
+	public static void main(String[] args) {
+		
+		
+		/*  int sum=0;
+		  
+		  for(int i=0;i<=15;i++) { 
+			  sum=sum+i;
+		  }
+		   System.out.println("Sum is "+sum); 
+		   
+		 }
+    }*/
+
+
+
+
+int sum=0;
+int i=1;
+while(i<=15) {
+	sum=sum+i;
+	i++;
+}
+System.out.println("Sum is "+sum);
+
+	}
+}
+
+		
+		
+	/*	
+	int i=0;
+	
+	do {
+		System.out.println("Sum is "+ i);
+		i++;
+	}while(i<=15);
+	
+	System.out.println("Sum is "+ i);
+	
+	
+	*/
+		
+		
+		
+		
+		
+		
+		
+	
+
+		
+		
+		
+		
+		
+		
+		
